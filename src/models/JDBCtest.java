f97944ca7600cfba45e6e09c8465cd758595ecc5
alexander <alexander.koronovskiy@gmail.com>
2019-02-19 15:39:07 +0400

    database with functions

--- src/models/JDBCtest.java
+++ src/models/JDBCtest.java
@@ -4,51 +4,69 @@ import java.sql.DriverManager;
 import java.sql.*;
 import java.util.logging.*;
 
-public class JDBCtest {
+class JDBCtest {
 
-    public static void main(String[] args) {
-        //подключение
-        Connection connection = null;
-        //URL к базе состоит из протокола:подпротокола://[хоста]:[порта_СУБД]/[БД] и других_сведений
-        String url = "jdbc:postgresql://localhost:5432/books";
-        //
-        String name = "postgres";
-        //Пароль
-        String password = "iron2479";
+    //URL к базе состоит из протокола:подпротокола://[хоста]:[порта_СУБД]/[БД] и других_сведений
+    String url = "jdbc:postgresql://localhost:5432/data";
+    String name = "postgres";
+    String password = "iron2479";
+    Connection connection = null;
+
+    public static void main(String args[]) {
+
+        String age = "15";
+        String city = "Orenburg";
+        String person = "Igor";
+        JDBCtest j = new JDBCtest();
+
         try {
-            //Загружаем драйвер
-            Class.forName("org.postgresql.Driver");
-            System.out.println("Драйвер подключен");
-            //Создаём соединение
-            connection = DriverManager.getConnection(url, name, password);
-            System.out.println("Соединение установлено");
-            Statement statement = null;
-            statement = connection.createStatement();
+            Connection connection = j.connectToDB();
+            j.isertData(connection,age,city,person);
+            j.printLast(connection);
+        } catch (Exception e){
+            System.out.println("Не могу выролнить запрос");
+        }
 
+    }
 
-            // обработка запроса
-            ResultSet result1 = statement.executeQuery(
-                    "SELECT * FROM company");
-            //result это указатель на первую строку с выборки
-            //чтобы вывести данные мы будем использовать
-            //метод next() , с помощью которого переходим к следующему элементу
-            System.out.println("Выводим statement");
-            while (result1.next()) {
-                System.out.println("Номер в выборке #" + result1.getRow()
-                        + "\t Номер в базе #" + result1.getInt("age")
-                        + "\t" + result1.getString("adress")); }
-
-        } catch (Exception ex) {
-            //выводим наиболее значимые сообщения
-            Logger.getLogger(JDBCtest.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException ex) {
-                    Logger.getLogger(JDBCtest.class.getName()).log(Level.SEVERE, null, ex);
-                }
+    public Connection connectToDB() {
+        Statement statement = null;
+            try {
+                Class.forName("org.postgresql.Driver");
+                System.out.println("Драйвер подключен");
+                //Создаём соединение
+                connection = DriverManager.getConnection(url, name, password);
+                System.out.println("Соединение установлено");
+            } catch (Exception e) {
+                System.out.println("Не удается соединиться с сервером");
             }
+        return connection;
+        }
+
+    public void isertData(Connection connection, String data1, String data2, String data3) {
+        try {
+            String sql = "INSERT INTO company VALUES (?,?,?)";
+            PreparedStatement stat = connection.prepareStatement(sql);
+            stat.setString(1, data1);
+            stat.setString(2, data2);
+            stat.setString(3, data3);
+            stat.executeUpdate();
+            System.out.println("Данные добавлены: " + data1 + " " + data2 + " " + data3);
+        } catch (Exception e){
+            System.out.println("Не могу выполнить запрос");
+            e.printStackTrace();
         }
     }
+
+    public void printLast(Connection connection){
+        try {
+            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+            ResultSet result1 = statement.executeQuery("SELECT * FROM company order by id desc limit 1;");
+            while (result1.next()) {
+                System.out.println(result1.getString("name") + " " + result1.getString("age"));
+            }
+        } catch (Exception e){e.printStackTrace();}
+    }
+
 }
